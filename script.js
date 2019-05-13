f3a47211ced7b498ba05174613b8f73e73ffa385
Agnieszka Matyja <agmatyja@gmail.com>
2019-05-14 01:29:56 +0200

    all classes work

--- script.js
+++ script.js
@@ -1,101 +1,112 @@
-/*var GalleryItem = React.createClass({
+var MoviePhoto = React.createClass({
   propTypes: {
-    image: React.PropTypes.object.isRequired,
+    photo: React.PropTypes.string.isRequired
   },
-
+  
   render: function() {
     return (
-      React.createElement('div', {},
-        React.createElement('h2', {}, this.props.image.name),
-        React.createElement('img', {src: this.props.image.src})
-      )
+      React.createElement('img', {src: this.props.photo})
     )
+  }
+});  
+var MovieTitle = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired 
   },
-});
-var element = React.createElement(GalleryItem);
-var image = {
-  name: 'Kotek',
-  src: 'http://imgur.com/n8OYCzR.png'
-};
+  
+  render: function() {
+    return (
+      React.createElement('h2', {}, this.props.title)
+    )
+  }
+});  
 
-var element = React.createElement(GalleryItem, {image: image});
-ReactDOM.render(element, document.getElementById('app'));
-*/
-
-
+var MovieDescription = React.createClass({
+  propTypes: {
+    desc: React.PropTypes.string.isRequired
+  },
+  
+  render: function() {
+    return (
+      React.createElement('p', {}, this.props.desc)
+    )
+  }
+});  
 var Movie = React.createClass({
-	propTypes: {
-      key: React.PropTypes.number.isRequired,
-      title: React.PropTypes.string.isRequired,
-      desc: React.PropTypes.string.isRequired,
-      photo: React.PropTypes.string.isRequired,
+  propTypes: {
+    key: React.PropTypes.number.isRequired,
+    title: React.PropTypes.string.isRequired,
+    desc: React.PropTypes.string.isRequired,
+    photo: React.PropTypes.string.isRequired,
+  },
+  
+  render: function() {
+    return (
+      React.createElement('li', {key: this.props.key},
+        React.createElement(MovieTitle, {title: this.props.title}),
+        React.createElement(MovieDescription, {desc: this.props.desc}),
+        React.createElement(MoviePhoto, {photo: this.props.photo})
+      )
+    );
+  }
+});  
+var MoviesList = React.createClass({
+  propTypes: {
+      movies: React.PropTypes.array.isRequired
     },
-	
-	render: function() {
-		return (
- 	      React.createElement('li', {key: this.props.key},
-			React.createElement('h2', {}, this.props.title),
-			React.createElement('p', {}, this.props.desc),
-			React.createElement('img', {src: this.props.photo})
-		  )
-		);
-	}
-	
-});	
+  
+  render: function() {
+    var moviesElements = this.props.movies.map(function(movie) {
+      return React.createElement(Movie, {
+        key: movie.id,
+        title: movie.title,
+        desc: movie.desc,
+        photo: movie.photo
+      });
+    });
+ 
+    return (
+      React.createElement('div', {},
+      React.createElement('h1', {}, 'Lista filmów'),
+      React.createElement('ul', {}, moviesElements)
+      )
+    );
+  }
+});  
 
-
-	
-	 var movies = [
+var movies = [
   {
     id: 1,
     title: 'Harry Potter',
     desc: 'film o czarodzieju',
-	photo:''
+    photo:''
   },
   {
     id: 2,
     title: 'Król Lew',
     desc: 'Film o królu sawanny',
-	photo:''
+    photo:''
   },
   {
     id: 3,
     title: 'Dzień świstaka',
     desc: 'Film wcale nie o świstaku',
-	photo:''
+    photo:''
   },
   {
     id: 4,
     title: 'Titanic',
     desc: 'Film o pechowym statku',
-	photo:''
+    photo:''
   },
   {
     id: 5,
     title: 'Chicago',
     desc: 'Film o strasznych babach',
-	photo:'pobrane.jpg'
-  },
-    
+    photo:'pobrane.jpg'
+  },  
  ];
-
-var moviesElements = movies.map(function(movie) {
-  return React.createElement(Movie, {
-	  key: movie.id,
-	  title: movie.title,
-	  desc: movie.desc,
-	  photo: movie.photo
-  });
-});
-
-
-var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-	);
+var element = React.createElement(MoviesList, {movies: movies});
+ReactDOM.render(element, document.getElementById('app'));
  
-
- ReactDOM.render(element, document.getElementById('app'));
  
-  //MovieTitle, MovieDescription, MoviesList; 
